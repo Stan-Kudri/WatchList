7628dc23816a3eff766b2228ffe267a50f292a0e
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-10-17 23:52:59 +0300

    Добавил MemoryFileProvider, который не был добавлен в прошлом коммите, из-за не сохранения файла.

--- TestUnitCore/MemoryFileProvider .cs
+++ TestUnitCore/MemoryFileProvider .cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Core.Repository.Provider;
 
 namespace TestUnitCore
 {
-    internal class MemoryFileProvider
+    public class MemoryFileProvider : IFileProvider
     {
+        private readonly NoDisposMemoryStream _memoryStream = new();
+
+        public Stream Open(string path, FileMode mode)
+        {
+            _memoryStream.Position = 0;
+            return _memoryStream;
+        }
+
+        public string ReadJson(string path)
+        {
+            var stream = Open(path, FileMode.Open);
+            using var streamRead = new StreamReader(stream);
+            return streamRead.ReadToEnd();
+        }
+
+        public MemoryFileProvider WriteJson(string path, string jsonDate)
+        {
+            var stream = Open(path, FileMode.Open);
+            using var streamWrite = new StreamWriter(stream);
+            streamWrite.Write(jsonDate);
+            return this;
+        }
+
+        private class NoDisposMemoryStream : MemoryStream
+        {
+            protected override void Dispose(bool disposing)
+            {
+            }
+        }
     }
 }
