5f1eda2104928d063373aef44ae443e009e74ccc
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-11-22 22:42:56 +0300

    EqualsWatchItemlTest:     Удалил массив с элементами и его логикой и в первый метод сравнивания двух одинаковых элементов используется один экземпляр класса.

--- EqualsTest/EqualsWatchItemlTest.cs
+++ EqualsTest/EqualsWatchItemlTest.cs
@@ -6,23 +6,18 @@ namespace EqualsTest
     public class EqualsWatchItemlTest
     {
         [Theory]
-        [MemberData(nameof(WatchItemDate))]
-        public void Equals_Two_Element_With_Same_Fields(List<WatchItem> items)
+        [MemberData(nameof(WatchItemElement))]
+        public void Equals_Two_Element_With_Same_Fields(WatchItem item)
         {
-            //Arrange
-            var сomparisonList = new List<bool>();
-
             //Act
-            foreach (var element in items)
-                сomparisonList.Add(element.Equals(element));
+            var comparison = item.Equals(item);
 
             //Assert
-            foreach (var compare in сomparisonList)
-                Assert.True(compare);
+            Assert.True(comparison);
         }
 
         [Theory]
-        [MemberData(nameof(WatchItemTwoElementsNotCompare))]
+        [MemberData(nameof(TwoNotEqualItem))]
         public void Equals_Two_Element_Not_With_Same_Fields(List<WatchItem> items)
         {
             //Arrange
@@ -127,23 +122,7 @@ namespace EqualsTest
             Assert.False(comparison);
         }
 
-        public static IEnumerable<object[]> WatchItemDate()
-        {
-            return new List<object[]>
-            {
-                new object[]
-                {
-                    new List<WatchItem>()
-                    {
-                        new ("Тор", 1, new DateTime(2022,08,10, 00, 00, 00), 8, TypeCinema.Movie, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7")),
-                        new ("Ход королевы", 1, null, null, TypeCinema.Series, Guid.Parse("5bbb70a3-0be8-4970-a95e-a5fbad2d1c72")),
-                        new ("Хоббит", 1, new DateTime(2022,03,22, 00, 00, 00), 10, TypeCinema.Movie, Guid.Parse("7a7d2f55-03e9-450e-955e-4e132c956d7e"))
-                    }
-                },
-            };
-        }
-
-        public static IEnumerable<object[]> WatchItemTwoElementsNotCompare()
+        public static IEnumerable<object[]> TwoNotEqualItem()
         {
             return new List<object[]>
             {
