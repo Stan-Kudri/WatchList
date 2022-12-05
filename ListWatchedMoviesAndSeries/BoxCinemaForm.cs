1f6a0e2b09332638470f89b4c0b95530f1ba1294
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-12-05 21:15:27 +0300

    Edit type Exception.

--- ListWatchedMoviesAndSeries/BoxCinemaForm.cs
+++ ListWatchedMoviesAndSeries/BoxCinemaForm.cs
@@ -268,7 +268,7 @@ namespace ListWatchedMoviesAndSeries
         {
             var countRowGridAllCinema = dgvCinema.RowCount;
             if (countRowGridAllCinema == 0)
-                throw new ArgumentException("Element not found");
+                throw new InvalidOperationException("Element not found");
             for (int i = 0; i < countRowGridAllCinema; i++)
             {
                 var titleItem = dgvCinema.Rows[i].Cells[IndexColumnId].Value;
