04a0f8e1d6b18a3cddf8fa2caa23ddbb25281a33
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-05-25 00:46:20 +0300

    WatchList.Test: WatchItemRepositoryTest - Add test method "Verifying_The_Use_Of_The_Update_Item_Method".

--- WatchList.Test/CoreTest/WatchItemRepositoryTest.cs
+++ WatchList.Test/CoreTest/WatchItemRepositoryTest.cs
@@ -11,7 +11,7 @@ namespace WatchList.Test.CoreTest
 {
     public class WatchItemRepositoryTest
     {
-        public static IEnumerable<object[]> WatchListItem() => new List<object[]>
+        public static IEnumerable<object[]> WatchListItemPage() => new List<object[]>
         {
             new object[]
             {
@@ -39,8 +39,30 @@ namespace WatchList.Test.CoreTest
             },
         };
 
+        public static IEnumerable<object[]> WatchListItemUpdate() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Залечь на дно в Брюгге", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 09, 10, 00, 00, 00), 10),
+                    new WatchItem("Джокер", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("68d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2020, 12, 10, 00, 00, 00), 10),
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                },
+
+                new WatchItem("Престиж", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 06, 10, 00, 00, 00), 10),
+
+                new List<WatchItem>()
+                {
+                    new WatchItem("Джокер", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("68d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2020, 12, 10, 00, 00, 00), 10),
+                    new WatchItem("Престиж", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 06, 10, 00, 00, 00), 10),
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                },
+            },
+        };
+
         [Theory]
-        [MemberData(nameof(WatchListItem))]
+        [MemberData(nameof(WatchListItemPage))]
         public void Verifying_The_Use_Of_The_Database_Page_Get_Method(
             List<WatchItem> watchItems,
             WatchItemSearchRequest searchRequest,
@@ -54,6 +76,26 @@ namespace WatchList.Test.CoreTest
             itemRepository.Add(watchItems);
             var pageItem = itemRepository.GetPageCinema(searchRequest);
             var actualItemPage = pageItem.Items;
+
+            actualItemPage.Should().Equal(expectWatchItems);
+        }
+
+        [Theory]
+        [MemberData(nameof(WatchListItemUpdate))]
+        public void Verifying_The_Use_Of_The_Update_Item_Method(
+            List<WatchItem> watchItems,
+            WatchItem updateItem,
+            List<WatchItem> expectWatchItems)
+        {
+            // Arrange
+            var appDbContext = new TestAppDbContextFactory().Create();
+            var itemRepository = new WatchItemRepository(appDbContext);
+            var searchRequest = new WatchItemSearchRequest();
+
+            // Act
+            itemRepository.Add(watchItems);
+            itemRepository.Update(updateItem);
+            var actualItemPage = itemRepository.GetPageCinema(searchRequest).Items;
 
             actualItemPage.Should().Equal(expectWatchItems);
         }
