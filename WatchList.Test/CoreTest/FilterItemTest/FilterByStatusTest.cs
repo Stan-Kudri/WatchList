fc2b3c028e613dec819b16cbf200670cedcdef29
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-05-27 13:38:41 +0300

    WatchList.Test: FilterByTypeTest and FilterByStatusTest - Fixed bugs in the formation and calling of tests.

--- WatchList.Test/CoreTest/FilterItemTest/FilterByStatusTest.cs
+++ WatchList.Test/CoreTest/FilterItemTest/FilterByStatusTest.cs
@@ -11,7 +11,7 @@ namespace WatchList.Test.CoreTest.FilterItemTest
 {
     public class FilterByStatusTest
     {
-        public static IEnumerable<object[]> WatchListItemAndFilter() => new List<object[]>
+        public static IEnumerable<object[]> ItemsToCheckFilterByStatusViewed() => new List<object[]>
         {
             new object[]
             {
@@ -22,78 +22,121 @@ namespace WatchList.Test.CoreTest.FilterItemTest
                     new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
                     new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
                     new WatchItem("Твое имя", 2, StatusCinema.Thrown, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7")),
-                    new WatchItem("Джокер", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("68d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2020, 12, 10, 00, 00, 00), 10),
+                },
+                StatusFilter.ViewedCinema,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
                 },
             },
         };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Status_Look(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByStatusViewed))]
+        public void List_0f_Filter_Items_By_Status_Viewed(List<WatchItem> watchItems, StatusFilter statusFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var statusFilter = StatusFilter.LookCinema;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, statusFilter);
-
             // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, statusFilter, expectWatchItems);
         }
+
+        public static IEnumerable<object[]> ItemsToCheckFilterByStatusPlanned() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Твое имя", 2, StatusCinema.Thrown, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                },
+                StatusFilter.PlannedWatchCinema,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                },
+            },
+        };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Status_Planned_Watch(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByStatusPlanned))]
+        public void List_0f_Filter_Items_By_Status_Planned_Watch(List<WatchItem> watchItems, StatusFilter statusFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var statusFilter = StatusFilter.PlannedWatchCinema;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, statusFilter);
-
             // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, statusFilter, expectWatchItems);
         }
+
+        public static IEnumerable<object[]> ItemsToCheckFilterByStatusThrown() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Твое имя", 2, StatusCinema.Thrown, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                },
+                StatusFilter.ThrownCinema,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Твое имя", 2, StatusCinema.Thrown, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                },
+            },
+        };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Status_Thrown(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByStatusThrown))]
+        public void List_0f_Filter_Items_By_Status_Thrown(List<WatchItem> watchItems, StatusFilter statusFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var statusFilter = StatusFilter.ThrownCinema;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, statusFilter);
-
             // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, statusFilter, expectWatchItems);
         }
+
+        public static IEnumerable<object[]> ItemsToCheckFilterByStatusLook() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Твое имя", 2, StatusCinema.Thrown, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                },
+                StatusFilter.LookCinema,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                },
+            },
+        };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Status_Viewed(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByStatusLook))]
+        public void List_0f_Filter_Items_By_Status_Look(List<WatchItem> watchItems, StatusFilter statusFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var statusFilter = StatusFilter.ViewedCinema;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, statusFilter);
-
             // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, statusFilter, expectWatchItems);
         }
 
-        private (List<WatchItem> actual, List<WatchItem> expect) TwoListsToCheckTheFilter(List<WatchItem> watchItems, StatusFilter statusFilter)
+        private void CheckListEqualAfterApplyFilter(List<WatchItem> watchItems, StatusFilter statusFilter, List<WatchItem> expectWatchItems)
         {
+            // Arrange
             var dbContext = new TestAppDbContextFactory().Create();
             var itemRepository = new WatchItemRepository(dbContext);
-            var watchItemSearchRequest = new WatchItemSearchRequest(new FilterItem(TypeFilter.AllCinema, statusFilter), SortField.Title, new Page(1, 6));
+            var watchItemSearchRequest = new WatchItemSearchRequest(new FilterItem(TypeFilter.AllCinema, statusFilter), SortField.Title, new Page());
 
-            var expectWatchItems = watchItems.Where(x => x.Status == statusFilter).ToList();
+            // Act
             itemRepository.Add(watchItems);
             var actualWatchItem = watchItemSearchRequest.ApplyFilter(dbContext.WatchItem).ToList();
 
-            return (actualWatchItem, expectWatchItems);
+            // Assert
+            actualWatchItem.Should().Equal(expectWatchItems);
         }
     }
 }
--- WatchList.Test/CoreTest/FilterItemTest/FilterByTypeTest.cs
+++ WatchList.Test/CoreTest/FilterItemTest/FilterByTypeTest.cs
@@ -11,7 +11,7 @@ namespace WatchList.Test.CoreTest.FilterItemTest
 {
     public class FilterByTypeTest
     {
-        public static IEnumerable<object[]> WatchListItemAndFilter() => new List<object[]>
+        public static IEnumerable<object[]> ItemsToCheckFilterByTypeMovie() => new List<object[]>
         {
             new object[]
             {
@@ -22,78 +22,117 @@ namespace WatchList.Test.CoreTest.FilterItemTest
                     new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
                     new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
                     new WatchItem("Твое имя", 2, StatusCinema.Viewed, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 09, 10, 00, 00, 00), 10),
-                    new WatchItem("Джокер", 2, StatusCinema.Viewed, TypeCinema.Movie, Guid.Parse("68d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2020, 12, 10, 00, 00, 00), 10),
+                },
+                TypeFilter.Movie,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
                 },
             },
         };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Movies(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByTypeMovie))]
+        public void List_0f_Filter_Items_By_Movies(List<WatchItem> watchItems, TypeFilter typeFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var typeFilter = TypeFilter.Movie;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, typeFilter);
-
-            // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, typeFilter, expectWatchItems);
         }
+
+        public static IEnumerable<object[]> ItemsToCheckFilterByTypeCartoon() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Твое имя", 2, StatusCinema.Viewed, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 09, 10, 00, 00, 00), 10),
+                },
+                TypeFilter.Cartoon,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                },
+            },
+        };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Cartoon(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByTypeCartoon))]
+        public void List_0f_Filter_Items_By_Cartoon(List<WatchItem> watchItems, TypeFilter typeFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var typeFilter = TypeFilter.Cartoon;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, typeFilter);
-
-            // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, typeFilter, expectWatchItems);
         }
+
+        public static IEnumerable<object[]> ItemsToCheckFilterByTypeSeries() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Твое имя", 2, StatusCinema.Viewed, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 09, 10, 00, 00, 00), 10),
+                },
+                TypeFilter.Series,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                },
+            },
+        };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Series(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByTypeSeries))]
+        public void List_0f_Filter_Items_By_Series(List<WatchItem> watchItems, TypeFilter typeFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var typeFilter = TypeFilter.Series;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, typeFilter);
-
-            // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, typeFilter, expectWatchItems);
         }
+
+        public static IEnumerable<object[]> ItemsToCheckFilterByTypeAnime() => new List<object[]>
+        {
+            new object[]
+            {
+                new List<WatchItem>()
+                {
+                    new WatchItem("Тор", 1, StatusCinema.Viewed, TypeCinema.Cartoon, Guid.Parse("18d4a732-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2013, 05, 11, 00, 00, 00), 8),
+                    new WatchItem("Хэнкок", 2, StatusCinema.Look, TypeCinema.Movie, Guid.Parse("28d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2015, 04, 12, 00, 00, 00), 9),
+                    new WatchItem("Человек-паук", 2, StatusCinema.Planned, TypeCinema.Series, Guid.Parse("38d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Драйв", 2, StatusCinema.Planned, TypeCinema.Movie, Guid.Parse("48d4a999-ca7a-4b24-a78a-65733ba419a7")),
+                    new WatchItem("Твое имя", 2, StatusCinema.Viewed, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 09, 10, 00, 00, 00), 10),
+                },
+                TypeFilter.Anime,
+                new List<WatchItem>()
+                {
+                    new WatchItem("Твое имя", 2, StatusCinema.Viewed, TypeCinema.Anime, Guid.Parse("58d4a999-ca7a-4b24-a78a-65733ba419a7"), new DateTime(2022, 09, 10, 00, 00, 00), 10),
+                },
+            },
+        };
 
         [Theory]
-        [MemberData(nameof(WatchListItemAndFilter))]
-        public void List_0f_Filter_Items_By_Anime(List<WatchItem> watchItems)
+        [MemberData(nameof(ItemsToCheckFilterByTypeAnime))]
+        public void List_0f_Filter_Items_By_Anime(List<WatchItem> watchItems, TypeFilter typeFilter, List<WatchItem> expectWatchItems)
         {
-            // Arrange
-            var typeFilter = TypeFilter.Anime;
-
-            // Act
-            var items = TwoListsToCheckTheFilter(watchItems, typeFilter);
-
-            // Assert
-            items.actual.Should().Equal(items.expect);
+            CheckListEqualAfterApplyFilter(watchItems, typeFilter, expectWatchItems);
         }
 
-        private (List<WatchItem> actual, List<WatchItem> expect) TwoListsToCheckTheFilter(List<WatchItem> watchItems, TypeFilter typeFilter)
+        private void CheckListEqualAfterApplyFilter(List<WatchItem> watchItems, TypeFilter typeFilter, List<WatchItem> expectWatchItems)
         {
+            // Arrange
             var dbContext = new TestAppDbContextFactory().Create();
             var itemRepository = new WatchItemRepository(dbContext);
-            var watchItemSearchRequest = new WatchItemSearchRequest(new FilterItem(typeFilter, StatusFilter.AllCinema), SortField.Title, new Page(1, 6));
+            var watchItemSearchRequest = new WatchItemSearchRequest(new FilterItem(typeFilter, StatusFilter.AllCinema), SortField.Title, new Page());
 
-            var expectWatchItems = watchItems.Where(x => x.Type == typeFilter).ToList();
+            // Act
             itemRepository.Add(watchItems);
             var actualWatchItem = watchItemSearchRequest.ApplyFilter(dbContext.WatchItem).ToList();
 
-            return (actualWatchItem, expectWatchItems);
+            // Assert
+            actualWatchItem.Should().Equal(expectWatchItems);
         }
     }
 }
