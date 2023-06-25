ea0be9953a7a83308680daad65193d40ec859200
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-06-26 00:05:39 +0300

    WatchList.Core: QuestionResultEnum - FIxed builder.

--- WatchList.Core/Model/QuestionResult/QuestionResultEnum.cs
+++ WatchList.Core/Model/QuestionResult/QuestionResultEnum.cs
@@ -2,10 +2,29 @@ namespace WatchList.Core.Model.QuestionResult
 {
     public enum QuestionResultEnum
     {
+        /// <summary>
+        /// Unknown button
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        /// "Yes" button
+        /// </summary>
         Yes,
+
+        /// <summary>
+        /// "Yes, for all" button
+        /// </summary>
         AllYes,
+
+        /// <summary>
+        /// "No" button
+        /// </summary>
         No,
-        AllNo
+
+        /// <summary>
+        /// "No, for all" button
+        /// </summary>
+        AllNo,
     }
 }
