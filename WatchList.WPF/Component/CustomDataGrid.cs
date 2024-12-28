83188f76479e80f468dc366dbcb10fd7c81ce351
Sergey <53861218+Stan-Kudri@users.noreply.github.com>
2024-12-28 23:05:26 +0300

    WatchList.WPF - Add resize columns DataGrid. (#113)

--- WatchList.WPF/Component/CustomDataGrid.cs
+++ WatchList.WPF/Component/CustomDataGrid.cs
@@ -7,7 +7,17 @@ namespace WatchList.WPF.Component
     public class CustomDataGrid : DataGrid
     {
         public CustomDataGrid()
-            => SelectionChanged += CustomDataGrid_SelectionChanged;
+        {
+            SelectionChanged += CustomDataGrid_SelectionChanged;
+            Loaded += (s, e) =>
+            { // Column widths
+                foreach (var column in Columns.AsParallel())
+                {
+                    column.MinWidth = column.ActualWidth;
+                    column.Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+                }
+            };
+        }
 
         public void CustomDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
             => SelectedItemsList = SelectedItems;
