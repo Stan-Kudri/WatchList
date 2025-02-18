56c310574fcaa3aaedb89758f59271788437f3ad
Sergey <53861218+Stan-Kudri@users.noreply.github.com>
2025-02-18 23:41:54 +0300

    Add implemented MessageBox (#136)
    * WatchList.Avalonia - Add default MessageBox
    
    * WatchList.Avalonia - Fixed bug updata items past updata item.
    
    * WatchList.Avalonia - Add MessageWindow "ShowDataReplaceQuestion" (not checked message)

---
+++ WatchList.Avalonia/Extension/MessageBoxReplaceItemExtension.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Dto;
+using MsBox.Avalonia.Models;
+using WatchList.Core.Model.QuestionResult;
+
+namespace WatchList.Avalonia.Extension
+{
+    public static class MessageBoxReplaceItemExtension
+    {
+        public static Task<DialogReplaceItemQuestion> GetDialogReplaceItem(this string titleItem)
+        {
+            var menager = MessageBoxManager.GetMessageBoxCustom(
+                                                                 new MessageBoxCustomParams
+                                                                 {
+                                                                     ButtonDefinitions = new List<ButtonDefinition>
+                                                                     {
+                                                                         new ButtonDefinition { Name = DialogReplaceItemQuestion.Yes.Name, },
+                                                                         new ButtonDefinition { Name = DialogReplaceItemQuestion.AllYes.Name, },
+                                                                         new ButtonDefinition { Name = DialogReplaceItemQuestion.No.Name, },
+                                                                         new ButtonDefinition { Name = DialogReplaceItemQuestion.AllNo.Name, }
+                                                                     },
+                                                                     ContentTitle = titleItem,
+                                                                     ContentMessage = "The append item is a duplicate.Replace element?",
+                                                                     Icon = MsBox.Avalonia.Enums.Icon.Question,
+                                                                     WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                                                                     CanResize = false,
+                                                                     MaxWidth = 400,
+                                                                     MaxHeight = 500,
+                                                                     SizeToContent = SizeToContent.WidthAndHeight,
+                                                                     ShowInCenter = true,
+                                                                     Topmost = false,
+                                                                 });
+            var messageShow = menager.ShowWindowAsync();
+            return messageShow.Result.ConvertToDialogReplaceItem();
+        }
+
+        private static Task<DialogReplaceItemQuestion> ConvertToDialogReplaceItem(this string? buttonDefinitionsName)
+        {
+            if (buttonDefinitionsName == DialogReplaceItemQuestion.Yes.Name)
+            {
+                return Task.FromResult(DialogReplaceItemQuestion.Yes);
+            }
+            else if (buttonDefinitionsName == DialogReplaceItemQuestion.AllYes.Name)
+            {
+                return Task.FromResult(DialogReplaceItemQuestion.AllYes);
+            }
+            else if (buttonDefinitionsName == DialogReplaceItemQuestion.No.Name)
+            {
+                return Task.FromResult(DialogReplaceItemQuestion.No);
+            }
+            else if (buttonDefinitionsName == DialogReplaceItemQuestion.AllNo.Name)
+            {
+                return Task.FromResult(DialogReplaceItemQuestion.AllNo);
+            }
+
+            return Task.FromResult(DialogReplaceItemQuestion.Unknown);
+        }
+    }
+}
--- WatchList.Avalonia/Views/Message/MessageWindow.cs
+++ WatchList.Avalonia/Views/Message/MessageWindow.cs
@@ -1,4 +1,8 @@
 using System.Threading.Tasks;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using WatchList.Avalonia.Data;
+using WatchList.Avalonia.Extension;
 using WatchList.Core.Model.QuestionResult;
 using WatchList.Core.Service.Component;
 
@@ -7,23 +11,41 @@ namespace WatchList.Avalonia.Views.Message
     public class MessageWindow : IMessageBox
     {
         public Task<DialogReplaceItemQuestion> ShowDataReplaceQuestion(string titleItem)
-            => Task.FromResult(DialogReplaceItemQuestion.AllYes);
+            => titleItem.GetDialogReplaceItem();
 
         public Task ShowError(string message)
         {
+            var box = MessageBoxManager.GetMessageBoxStandard(Constans.ErrorMessage, message, ButtonEnum.Ok);
+            var result = box.ShowAsync();
             return Task.CompletedTask;
         }
 
         public Task ShowWarning(string message)
-            => Task.CompletedTask;
+        {
+            var box = MessageBoxManager.GetMessageBoxStandard(Constans.WarningMessage, message, ButtonEnum.Ok);
+            var result = box.ShowAsync();
+            return Task.CompletedTask;
+        }
 
         public Task ShowInfo(string message)
-            => Task.CompletedTask;
+        {
+            var box = MessageBoxManager.GetMessageBoxStandard(Constans.InformationMessage, message, ButtonEnum.Ok);
+            var result = box.ShowAsync();
+            return Task.CompletedTask;
+        }
 
-        public Task<bool> ShowQuestion(string message)
-            => Task.FromResult(true);
+        public async Task<bool> ShowQuestion(string message)
+        {
+            var box = MessageBoxManager.GetMessageBoxStandard(Constans.QuestionMessage, message, ButtonEnum.YesNo);
+            var result = await box.ShowWindowAsync();
+            return await (result == ButtonResult.Yes ? Task.FromResult(true) : Task.FromResult(false));
+        }
 
-        public Task<bool> ShowQuestionSaveItem(string message)
-            => Task.FromResult(true);
+        public async Task<bool> ShowQuestionSaveItem(string message)
+        {
+            var box = MessageBoxManager.GetMessageBoxStandard(Constans.QuestionMessage, message, ButtonEnum.OkCancel);
+            var result = await box.ShowWindowAsync();
+            return await (result == ButtonResult.Ok ? Task.FromResult(true) : Task.FromResult(false));
+        }
     }
 }
